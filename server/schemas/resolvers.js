8e4dfe2c81dd948ef86726dcba36c43a15548dfd
Alan Phelps <phelpsa64@gmail.com>
2022-04-25 19:12:39 -0400

    set up schemas files

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,9 @@
+const { User, Book } = require("../models");
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
+
+const resolvers = {
+
+};
+
+module.exports = resolvers;
