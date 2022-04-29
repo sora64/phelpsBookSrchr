14764875f987ce164534a2a71e674d03228ec28d
Alan Phelps <phelpsa64@gmail.com>
2022-04-29 01:30:58 -0400

    fixed a typo in queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,25 +1,7 @@
 import { gql } from "@apollo/client";
 
-// export const GET_ME = gql`
-//   {
-//     me {
-//       _id
-//       username
-//       email
-//       savedBooks {
-//         bookId
-//         authors
-//         title
-//         description
-//         image
-//         link
-//       }
-//     }
-//   }
-// `;
-
 export const GET_ME = gql`
-  query me {
+  {
     me {
       _id
       username
